7c7abb21a278b47daf60dc588e291426f3e8a594
XiaoruiLuo <690722590@qq.com>
2025-12-17 22:42:25 +0800

    udpate

--- handler/auth.go
+++ handler/auth.go
@@ -27,29 +27,14 @@ func (u *Auth) Login(c *gin.Context) {
 		return
 	}
 
-	// 1. code 换 openid
 	wxResp, err := u.WeChatService.Code2Session(c.Request.Context(), req.LoginCode)
 	if err != nil {
-		c.JSON(500, gin.H{"error": "微信登录失败"})
+		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
-	// 2. TODO: 根据 openid 查用户 / 创建用户
-	userID := int64(12345) // 示例
-
-	// 3. TODO: 生成你自己的 token
-	token := "123"
-
 	c.JSON(200, gin.H{
-		"code": 0,
-		"msg":  "登录成功",
-		"data": gin.H{
-			"openid":      wxResp.OpenID,
-			"is_new_user": true,
-			"user_id":     userID,
-			"token":       token,
-			"data":        wxResp,
-		},
+		"data": wxResp,
 	})
 }
 
@@ -65,7 +50,6 @@ func (u *Auth) BindPhone(c *gin.Context) {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
-	// 3. TODO: 绑定手机号到当前登录用户（user_id 从 token 中取）
 	phone := userPhoneNumber
 	c.JSON(200, gin.H{
 		"code": 0,
