aa914b126c530351edb5fda6cea76fb1d802b566
luosmallrui <luoxiaorui115815@qq.com>
2025-12-23 17:56:19 +0800

    update

--- service/map.go
+++ service/map.go
@@ -14,12 +14,6 @@ type IMapService interface {
 	GetMapData() (interface{}, error)
 }
 
-func NewMapService(mapDao *dao.MapDao) *MapService {
-	return &MapService{
-		MapDao: mapDao,
-	}
-}
-
 // GetMapData 获取地图数据
 func (s *MapService) GetMapData() (interface{}, error) {
 	return s.MapDao.GetMapData()
