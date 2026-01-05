00480d326695f85569fbe1ff7f82f35f1eabaf26
manchaohuan <manchaohuan@qq.com>
2026-01-05 14:53:48 +0800

    feat:like

--- dao/wire.go
+++ dao/wire.go
@@ -2,20 +2,19 @@
 
 package dao
 
-import "github.com/google/wire"
+import (
+	"github.com/google/wire"
+)
 
 var ProviderSet = wire.NewSet(
 	NewAdmin,
 	NewUsers,
 	NewMapDao,
 	NewNoteDAO,
-<<<<<<< HEAD
 	NewMessageDAO,
 	NewGroupDAO,
 	NewMessageReadDAO,
 	NewGroupMember,
-=======
 	NewNoteLikeDAO,
 	NewNoteStatsDAO,
->>>>>>> 3bc8e5d (实现点赞、取消点赞、查询点赞次数、查询用户自己是否已经点赞接口)
 )
