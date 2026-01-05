5eec85cbecb3a5b6ca0ed22d352bf8da34e0fd2c
manchaohuan <manchaohuan@qq.com>
2026-01-05 10:03:26 +0800

    实现点赞、取消点赞、查询点赞次数、查询用户自己是否已经点赞接口

--- dao/wire.go
+++ dao/wire.go
@@ -9,8 +9,13 @@ var ProviderSet = wire.NewSet(
 	NewUsers,
 	NewMapDao,
 	NewNoteDAO,
+<<<<<<< HEAD
 	NewMessageDAO,
 	NewGroupDAO,
 	NewMessageReadDAO,
 	NewGroupMember,
+=======
+	NewNoteLikeDAO,
+	NewNoteStatsDAO,
+>>>>>>> 3bc8e5d (实现点赞、取消点赞、查询点赞次数、查询用户自己是否已经点赞接口)
 )
