753942110f536608e6f1c7e10463399f5f8fc7ea
XiaoruiLuo <690722590@qq.com>
2026-01-13 20:53:03 +0800

    feat:note

--- pkg/utils/convert.go
+++ pkg/utils/convert.go
@@ -1,45 +1,8 @@
 package utils
 
 import (
-	"Hyper/models"
-	"Hyper/types"
 	"encoding/json"
 )
-
-func ConvertNoteModelToDTO(note *models.Note) (*types.Notes, error) {
-	if note == nil {
-		return nil, nil
-	}
-
-	dto := &types.Notes{
-		ID:          int64(note.ID),
-		UserID:      int64(note.UserID),
-		Title:       note.Title,
-		Content:     note.Content,
-		Type:        note.Type,
-		Status:      note.Status,
-		VisibleConf: note.VisibleConf,
-		CreatedAt:   note.CreatedAt,
-		UpdatedAt:   note.UpdatedAt,
-	}
-
-	// topic_ids
-	if err := unmarshalJSON(note.TopicIDs, &dto.TopicIDs); err != nil {
-		dto.TopicIDs = make([]int64, 0)
-	}
-
-	// location
-	if err := unmarshalJSON(note.Location, &dto.Location); err != nil {
-		dto.Location = types.Location{}
-	}
-
-	// media_data
-	if err := unmarshalJSON(note.MediaData, &dto.MediaData); err != nil {
-		dto.MediaData = make([]types.NoteMedia, 0)
-	}
-
-	return dto, nil
-}
 
 func unmarshalJSON[T any](src string, dst *T) error {
 	if src == "" {
