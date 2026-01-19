0d8f5059df3b1a3b107efbbeb76cd3d3cfccc4e7
AoXiang2003 <2068034701@qq.com>
2026-01-19 22:36:11 +0800

    feat: 增加群禁言功能（个人禁言/群禁言）并完善文档

--- types/session.go
+++ types/session.go
@@ -7,7 +7,7 @@ type SessionDTO struct {
 	LastMsgTime int64  `json:"last_msg_time"`
 	Unread      uint32 `json:"unread"`
 	IsTop       int    `json:"is_top"`
-	IsMute      int    `json:"is_mute"`
+	IsMute      int    `json:"is_mute"` //这是会话免打扰，不是禁言，只不过同名了
 	PeerAvatar  string `json:"peer_avatar"`
 	PeerName    string `json:"peer_name"`
 }
