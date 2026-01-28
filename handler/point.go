e3e694c05c266d50700fdf387edf46eeef155f44
luosmallrui <luoxiaorui115815@qq.com>
2026-01-29 01:08:37 +0800

    upload

--- handler/point.go
+++ handler/point.go
@@ -8,7 +8,6 @@ import (
 	"Hyper/service"
 	"Hyper/types"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,52 +20,11 @@ type PointHandler struct {
 func (p *PointHandler) RegisterRouter(r gin.IRouter) {
 	authorize := middleware.Auth([]byte(p.Config.Jwt.Secret))
 	pointGroup := r.Group("/v1/points")
-	pointGroup.GET("/balance", context.Wrap(p.Balance))
 	pointGroup.GET("/records", authorize, context.Wrap(p.GetRecordsItem))
 	pointGroup.POST("/reward", context.Wrap(p.RewardPoints))
 	pointGroup.POST("/consume", authorize, context.Wrap(p.ConsumePoint))
-	pointGroup.GET("/getallpoint", authorize, context.Wrap(p.GetAccountAllPoint))
+	pointGroup.GET("/balance", authorize, context.Wrap(p.GetAccountAllPoint))
 
-}
-
-func (p *PointHandler) Balance(c *gin.Context) error {
-
-	resp := types.PointsAccount{
-		Balance:       3000,
-		TotalEarned:   5000,
-		TotalUsed:     2000,
-		PendingCount:  3,
-		PendingAmount: 20,
-	}
-	response.Success(c, resp)
-	return nil
-}
-
-func (p *PointHandler) GetRecords(c *gin.Context) error {
-	l := make([]types.PointRecord, 0)
-	l = append(l, types.PointRecord{
-		ID:          1,
-		Amount:      80,
-		Description: "消费（门票）",
-		OrderType:   "ticket",
-		CreatedAt:   time.Now().Format("2006-01-02 15:04:05"),
-		Status:      1,
-	})
-	l = append(l, types.PointRecord{
-		ID:          2,
-		Amount:      -190,
-		Description: "积分使用（汽车改装）",
-		OrderType:   "car_service",
-		CreatedAt:   time.Now().Format("2006-01-02 15:04:05"),
-		Status:      1,
-	})
-	resp := types.ListPointsRecord{
-		Records:    l,
-		NextCursor: 0,
-		HasMore:    false,
-	}
-	response.Success(c, resp)
-	return nil
 }
 
 func (p *PointHandler) RewardPoints(c *gin.Context) error {
